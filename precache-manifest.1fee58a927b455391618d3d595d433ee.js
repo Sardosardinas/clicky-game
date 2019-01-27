dc16937f5754cc20bb3aec10f748229c18819863
Alix Reyna <areyna@riotgames.com>
2019-01-26 20:44:38 -0600

    Updates

---
+++ precache-manifest.1fee58a927b455391618d3d595d433ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "baec8e28be6f487c3e3a",
+    "url": "/clicky-game/static/js/main.baec8e28.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "baec8e28be6f487c3e3a",
+    "url": "/clicky-game/static/css/main.01ea16dc.chunk.css"
+  },
+  {
+    "revision": "60b6085a79450805e17042164085a3a1",
+    "url": "/clicky-game/index.html"
+  }
+];
